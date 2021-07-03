7c8a62af54c2b026b7e05f34e3abfbcd8d489006
Thierry Uwilingiyimana <thierry.uwi@gmail.com>
2021-07-03 18:16:59 -0400

    spacing

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@
 // "Debugging is twice as hard as writing the code in the first place.
 // Therefore, if you write the code as cleverly as possible,
 // you are, by definition, not smart enough to debug it."
-// 
+//
 // - KR
 
 //  Cortney's guidance: user action => add event listener w/ callback
