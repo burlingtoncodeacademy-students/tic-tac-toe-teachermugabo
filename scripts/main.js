afed145986db9ed406e403ce4eecea7c7b9090a3
Thierry Uwilingiyimana <thierry.uwi@gmail.com>
2021-07-03 18:16:11 -0400

    did too much here - massive reorganizing + refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,108 @@
-/**
-* "Debugging is twice as hard as writing the code in the first place.
-* Therefore, if you write the code as cleverly as possible,
-* you are, by definition, not smart enough to debug it."
-* 
-* - KR
-*/
+/* ------------ GAME IMPLEMENTATION NOTES ------------------- */
 
-//  remember Cortney's thinking: user action => add event listener w/ callback
+// Story: No Rules [done]
+// Story: Rules [done]
+// Story: Winning Condition [done]
+// Story: Reset [done]
+// Story: Replay [done]
+// Story: Multiplayer [done?] -- this is the default
+// Story: Names [not started]
+// Story: Single-player [not started] -- random computer
+
+
+/* -------------- GENERAL GUIDING WISDOM ------------------- */
+
+// "Debugging is twice as hard as writing the code in the first place.
+// Therefore, if you write the code as cleverly as possible,
+// you are, by definition, not smart enough to debug it."
+// 
+// - KR
+
+//  Cortney's guidance: user action => add event listener w/ callback
 //  that callback sets another callback or calls another method. 
 //  we end up with a chain of functionality.
 
-//  MOTTO: MAKE IT WORK, THEN RIGHT, THEN FAST
+//  GENERAL BOOTCAMP MOTTO: MAKE IT WORK, THEN RIGHT, THEN FAST
 
-// Story: Start Game [done]
-// =====================
-// Given an empty board & init game variables
 
-// 3x3 matrix to represent game state
-// Player moves will be represented with 
-// PLAYER_O_STATE and PLAYER_X_STATE. 
-let gameState = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
+/* ---------   GAME STATE GLBOAL CONSTANTS & VARIABLES --------- */
+
+// Global Variables
+let gameState = [[],[],[]] // 3x3 matrix to represent game state
+let gameOver = false
+
+// Global References to board pieces
+let gameStatus = document.getElementById('status') // reference to game status display
+let cells = document.querySelectorAll('.board>.block') // board cells (board blocks in CSS)
+
+// Global references to game buttons
+let startBtn = document.getElementById('startBtn') // start button
+let resetBtn = document.getElementById('resetBtn') // reset button
+let replayBtn = document.getElementById('replayBtn') // replay button
+
+// TIC-TAC-TOE winning condition representations
+const boardLines = [
+    // rows wins
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    
+    // columns wins
+    [0,3,6],
+    [1,4,7],
+    [2,5,8], 
+
+    // diagonals wins
+    [0,4,8],
+    [2,4,6]
 ]
 
 // values to represent players moves in gameState
 const PLAYER_O_STATE = -1
 const PLAYER_X_STATE = 1
 
-// global indicators for whether game is over
-let gameOver = false
 
-// game status display
-let gameStatus = document.getElementById('status')
+/* ------------ INITIALIZE GAME STATE & UI --------------- */
 
-// initialize game blocks (the cells)
-let cells = document.querySelectorAll('.board>.block')
+/**
+ * Name: clearBoardCells
+ * =====================
+ * Iterates thru board cells and sets them to empty string
+ * * Operates on the Global variable cells
+ * 
+ * @param none
+ * @returns none
+ */
+ const clearBoardCells = () => {
+    for(let cell of cells) {
+        cell.textContent = ''
+    }
+}
 
-// initialize game buttons
-let startBtn = document.getElementById('startBtn')
-// startBtn.disabled = "false" // default behavior
+/**
+ * Name: initGame
+ * ==============
+ * Initialize game state
+ */
+ const initGame = () => {
+    // console.log("entering init game")
 
-let resetBtn = document.getElementById('resetBtn')
-resetBtn.disabled = "true"
+    gameStatus.textContent = "Click Start to Play" // init game status
+    gameState = [  // clear game state
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]] 
+    clearBoardCells() // clear the board
 
-let replayBtn = document.getElementById('replayBtn')
-replayBtn.disabled = "true"
+    // enable start button, disable the rest. 
+    if( startBtn.disabled ) startBtn.disabled = false
+    resetBtn.disabled = "true" // disabled 
+    replayBtn.disabled = "true" // disabled
+}
 
+initGame() // init game & board on page load
+
+/* ------------ USER ACTIONS IMPLEMENTATION --------------- */
 
 /**
  * Name: Start Game
@@ -57,95 +112,109 @@ replayBtn.disabled = "true"
  * When the user
  * @param {*} event 
  */
-const startGame = (event) => {
-    // Then the 'Start Game' button is disabled ("grayed out")
-    startBtn.disabled = "true"
-
-    // And the status area contains "Player X's turn
-    gameStatus.textContent = "Player X's turn"
+ const startGame = (event) => {
+    gameStatus.textContent = "Player X's turn" // Player X starts first
+    startBtn.disabled = "true" // disable 'Start Game' button to start the game
 }
 
-// When the user clicks 'Start Game'
-startBtn.addEventListener('click', startGame)
-
-
 /**
- * Handler to replay the game
- * Combines the functionality of resetBtn & startBtn
+ * Name: replayGame
+ * ================
+ * Handler to replay the game.
+ * Combines functionality of reset & start actions. 
+ * 
+ * @param {*} event 
  */
- replayBtn.addEventListener("click", (event) => {
-    gameReset(event) // // First, reset the game
-
-    // ? is startBtn.click() possible?
-    gameStatus.textContent = "Player X's turn" // // player X starts firsts
-    replayBtn.disabled = "true"
-})
-
+ const replayGame = (event) => {
+    initGame() // First, reset the game
+    startGame() // then start next game
+}
 
 /**
  * Name: gameReset
- * Resets game state and clears the board.
+ * Resets game state and clears the board, 
+ * and prepares board for next game.
+ * Player is invite to Start the game.
  * 
  * @param {*} event 
  */
 const gameReset = (event) => {
-    // ? is resetBtn.click() possible?
-    gameState = [] // clear game state
-    clearBoardCells() // clear the board
-    startBtn.disabled = true // disable start button
-    resetBtn.disabled = true // disable reset button
-    gameOver = false // set game is over to false
-
-    gameStatus.textContent = "Player X's turn"  // player X starts firsts
+    initGame()
 }
+
+/**
+ * Name: selectCell
+ * ================
+ * Called whenever a player makes a move (selects a cell).
+ * Makes sure the game has started, that it is not over, 
+ * and that the cell isn't already filled.
+ * 
+ * When those conditions are met, update cell & check for win.
+ * 
+ * @param {*} event 
+ */
+const selectCell = (event) => {
+    let cell = event.target
+
+    // check if game has started
+    if( startBtn.disabled ) {
+
+        // once game starts, make sure resetBtn is enabled.
+        if ( resetBtn.disabled ) resetBtn.disabled = false
+
+        // check if game isn't over yet
+        if (gameOver) {
+            alert("Game is over. Click Replay to play again!")
+        }
+        // When user selects a cell that is not empty
+        else if ( cell.textContent != '') {
+            // Then the game says "Please select an empty cell." 
+            // and does not put an X or O in the cell
+            alert("That one is taken! Find an empty cell.")
+        } 
+        // we have a valid move - update cell & check if it's a winner!
+        else {
+            updateCell(cell)
+            checkWinCondition() // use public arrayBoard
+        }
+    }
+    // if game hasn't started, tell 'em
+    else {
+        alert("Gotta start the game to play. Click the start button :-)")
+    }
+    
+    console.log('gameState', gameState)
+}
+
+
+/* ------------ USER ACTION HANDLERS --------------------- */
+
+// When the user clicks 'Start Game'
+startBtn.addEventListener('click', startGame)
+
+// When the user clicks 'Replay'
+replayBtn.addEventListener("click", replayGame)
 
 // When user clicks 'Reset'
 resetBtn.addEventListener("click", gameReset)
 
-
-// Story: No Rules [done]
-// Story: Rules [done]
-// Story: Winning Condition [wip]
-// ==============================
-
-// Given an empty board, and the current player is X
 // When the user selects a cell
 for(let cell of cells) {
-    cell.addEventListener('click', (event) => {
-        let cell = event.target
-
-        // check if game has started
-        if( startBtn.disabled ) {
-
-            // once game starts, make sure resetBtn is enabled.
-            if ( resetBtn.disabled ) resetBtn.disabled = false
-
-            // check if game isn't over yet
-            if (gameOver) {
-                alert("Game is over. Click Replay to play again!")
-            }
-            // When user selects a cell that is not empty
-            else if ( cell.textContent != '') {
-                // Then the game says "Please select an empty cell." 
-                // and does not put an X or O in the cell
-                alert("That one is taken! Find an empty cell.")
-            } else {
-                updateCell(cell)
-                checkWinCondition() // use public arrayBoard
-            }
-        }
-        // if game hasn't started, tell 'em
-        else {
-            alert("Gotta start the game to play. Click the start button :-)")
-        }
-        
-        console.log('gameState', gameState)
-    })
+    cell.addEventListener('click', selectCell)
 }
 
-// Redrawing the board
-// given a cell, update the global gameStatus variable
-// to determine who's move it is, and update the board
+
+
+/* ------------ GAME HELPER FUNCTIONS ------------------- */
+
+/**
+ * Name: updateCell
+ * ================
+ * Given a cell, check the global gameStatus variable,
+ * to determine who's move it is, and update the board
+ * 
+ * @param {*} cell 
+ */
 const updateCell = (cell) => {
     
     // Given the player is X
@@ -178,22 +247,6 @@ const updateCell = (cell) => {
         gameStatus.textContent = "Player X's turn" 
     }
 }
-
-let boardLines = [
-    // rows
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    
-    // columns
-    [0,3,6],
-    [1,4,7],
-    [2,5,8], 
-
-    // diagonals
-    [0,4,8],
-    [2,4,6]
-]
 
 /**
  * Check Win Condition
@@ -248,6 +301,15 @@ const isBoardFull = () => {
     return true;
 }
 
+/**
+ * Name: endGame
+ * =============
+ * Call method to end the game (sets global variable gameOver)
+ * Highlight the winning line if game was won (could've been a draw)
+ * Update buttons to set user up for a replay :-) 
+ * 
+ * @param {[#,#,#]} line 
+ */
 const endGame = (line)  => {
     // highlight winning line, if there is one.     
     // if (line) highlightLine(line) 
@@ -256,21 +318,6 @@ const endGame = (line)  => {
 
     resetBtn.disabled = true // disable reset button
     replayBtn.disabled = false // enable replay button
-}
-
-/**
- * Name: clearBoardCells
- * =====================
- * Iterates thru board cells and sets them to empty string
- * * Operates on the Global variable cells
- * 
- * @param none
- * @returns none
- */
-const clearBoardCells = () => {
-    for(let cell of cells) {
-        cell.textContent = ''
-    }
 }
 
 /**
@@ -283,11 +330,9 @@ const clearBoardCells = () => {
  * This method is used to determine win conditions.
  * 
  * @param {[#,#,#]} line 
- * @param {[
-        [#,#,#],
-        [#,#,#],
-        [#,#,#]
-    ]} gameState 
+ * @param {[[#,#,#],
+            [#,#,#],
+            [#,#,#]]} gameState 
  * @return sum of corresponding board cells
  */
 const sumBoardLine = (line, gameState) => {
